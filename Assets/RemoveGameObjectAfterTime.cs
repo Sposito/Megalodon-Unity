2a59ad5ba7b4b6c6eae7542d04c6797b4d29a822
Sposito <sposito.thiago@gmail.com>
2015-11-12 18:38:47 -0200

    Object Timer Remover
    Component used in the Bubble Animator

---
+++ Assets/RemoveGameObjectAfterTime.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+/// <summary>
+/// The Game Object this component is attached to is removed after a given time in seconds;
+/// </summary>
+public class RemoveGameObjectAfterTime : MonoBehaviour {
+
+	public float seconds = 3f;
+	void Start () {
+		StartCoroutine ("Remove");
+	}
+	
+	IEnumerator Remove(){
+		yield return new WaitForSeconds (seconds);
+		Destroy (gameObject);
+		print (5);
+	}
+}
