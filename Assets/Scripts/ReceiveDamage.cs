515cb9526923aae2e063c07ccb16bf32bfb18451
Sposito <sposito.thiago@gmail.com>
2015-11-30 10:58:37 -0200

    ReceiveDamage enum
    Receive Damage uses an enum to set its kind

--- Assets/Scripts/ReceiveDamage.cs
+++ Assets/Scripts/ReceiveDamage.cs
@@ -1,24 +1,56 @@
-﻿using UnityEngine;
+﻿
+using UnityEngine;
 using System.Collections;
 
 public class ReceiveDamage : MonoBehaviour {
 
+	public enum DamageableKind {PowerUp, Boss, Player, Other};
+
+	public DamageableKind kind = DamageableKind.PowerUp;
 	public GameObject particles;
-	public AudioSource sound;
+	private AudioSource sound;
 
 	public int maxHits = 1;
 	public int hits;
 
-	public bool boss;
-	public bool player;
+	private bool boss;
+	private bool player;
+	private bool powerUp;
+	private bool other;
 
 	void Start () {
 		sound = GameObject.Find ("BubblesSound").GetComponent<AudioSource>();
+		SetKind ();
 	}
 	
 
 	void Update () {
+#if UNITY_EDITOR
 	
+#endif
+	
+	}
+
+	void SetKind(){
+		boss = false;
+		player = false;
+		powerUp = false;
+		other =  false;
+
+		switch (kind) {
+			case DamageableKind.Boss:
+				boss = true;
+				return;
+			case DamageableKind.Player:
+				player = true;
+				return;
+			case DamageableKind.PowerUp:
+				powerUp = true;
+				return;
+			default:
+				other = true;
+				return;
+		}
 	}
 
 	void OnTriggerEnter2D(Collider2D other){
