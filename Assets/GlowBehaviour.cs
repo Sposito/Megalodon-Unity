9156880a7d0c10d3065312faff14524544233049
Sposito <sposito.thiago@gmail.com>
2015-12-06 23:08:09 -0200

    Anemona and glow setup

---
+++ Assets/GlowBehaviour.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GlowBehaviour : MonoBehaviour {
+
+	SpriteRenderer sR;
+	Color colorIni;
+	public float amplitude = 0.5f;
+	bool movingToRight = true;
+	float speed = 0.01f;
+	bool move = true;
+	bool up = true;
+
+	Color target;
+
+	void Start () {
+		sR = GetComponent<SpriteRenderer> ();
+		colorIni = sR.color;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		RunRange ();
+	}
+
+	void RunRange(){
+		sR.color = Color.Lerp (sR.color, target, 0.01f);
+
+		if (sR.color.a > 0.5 && up) {
+			target = new Color (colorIni.r, colorIni.g, colorIni.b, 0f);
+			up = false;
+		}
+		if (sR.color.a <= 0.1f && !up) {
+			target = new Color (colorIni.r, colorIni.g, colorIni.b, .5f);
+			up = true;
+		}
+
+	}
+}
---
+++ Assets/StarAtRandomFrame.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StarAtRandomFrame : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Animator anim = GetComponent<Animator>();
+		AnimatorStateInfo state = anim.GetCurrentAnimatorStateInfo(0);
+		anim.Play(state.fullPathHash, -1, Random.Range(0f, 1f));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
